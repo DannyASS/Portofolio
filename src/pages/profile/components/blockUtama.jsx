bf212642029e3c754aa0b997bb39d15235abd374
Danny Anthonyo <danthony@PMINTL.NET>
2024-05-23 13:05:35 +0700

    Feat : add Profile Page

---
+++ src/pages/profile/components/blockUtama.jsx
@@ -0,0 +1,25 @@
+import { Container, Typography } from "@mui/material"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../style.scss'
+
+const BlockPage = () => {
+    return(
+        <Container>
+            <div>
+                <img className='image' src="/image/profile.jpg" alt="" />
+            </div>
+            <Typography variant="h3" component="h1" gutterBottom>
+                My Profile
+            </Typography>
+            <div className="card shadow border">
+                <div className="card-body">
+                <h5 className="card-title">Danny Anthonyo Sattu Sailo</h5>
+                <h6 className="card-subtitle mb-2 text-muted">Software Engineer</h6>
+                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+        </Container>
+    )
+}
+
+export default BlockPage;
---
+++ src/pages/profile/profile.jsx
@@ -0,0 +1,22 @@
+// File: src/components/ProfilePage.jsx
+
+import React from 'react';
+import { Container } from '@mui/material';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Col, Row } from 'react-bootstrap';
+import BlockPage from './components/blockUtama';
+
+const ProfilePage = () => {
+  return (
+        <Row>
+            <Col>
+                <BlockPage />
+            </Col>
+            <Col>
+            
+            </Col>
+        </Row>
+  );
+};
+
+export default ProfilePage;
