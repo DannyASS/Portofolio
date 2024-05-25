185cca757840e3758283aa07cbdd957057c2f697
Danny Anthonyo <danthony@PMINTL.NET>
2024-05-25 23:43:55 +0700

    Fix : logo

--- src/components/shared/navbar.jsx
+++ src/components/shared/navbar.jsx
@@ -3,15 +3,17 @@ import { Container } from "react-bootstrap"
 import { convertObjectToArray } from "../../utils/router/objectRouter"
 import { Navigation, Widgets } from "@mui/icons-material";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Sidebar = () => {
+    const [collapsed, setCollapsed] = useState(false);
     const dataMenu = convertObjectToArray();
-    const {Header, Sider} = Layout
+    const {Sider} = Layout
     console.log("cek object :", dataMenu);
     return(
-        <Sider className="sideBar">
-            <div className="iconNav">
-                <Widgets />
+        <Sider collapsed={collapsed} className="sideBar">
+            <div className="iconNav" onClick={() => setCollapsed(!collapsed)}>
+                <img className="logoDas" src="/image/programmer.png"/>
             </div>            
             <Menu theme="dark">
                 {dataMenu.map((item)=> {
